ffb7b055937f1ab05438e24556d7af2be45e22ad
alegsu <alessandro@shinyup.it>
2026-06-26 14:28:55 +0200

    fix: remove duplicate React import that caused build failure

--- src/components/KanbanView.js
+++ src/components/KanbanView.js
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import React, { useState, useEffect } from 'react';
 import { flushSync } from 'react-dom';
 import CardModal from './CardModal';
 import styles from './KanbanView.module.css';
