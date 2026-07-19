cdfe031ba0872aef3618e12e606421120b5ef13b
alegsu <alessandro@shinyup.it>
2026-07-19 18:51:05 +0200

    chore: update page title

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Trello Calendar Dashboard",
+  title: "GestionAle | Il gestionale di ShinyUp",
   description: "Advanced resource management dashboard for Trello and Google Calendar",
 };
 
